de8f0b8cba73ddb05d975d6541a04fb27294086f
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-09 18:02:12 -0400

    Tested and updated rate limiter

--- server/middleware/rateLimiter.js
+++ server/middleware/rateLimiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 20, // limit each IP to 20 requests per windowMs
   message: {
     status: 429,
     message: "Too many requests from this IP, please try again later.",
