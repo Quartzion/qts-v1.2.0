24fc66b6e03e5e6231d5b0b93838030b6fdeb3c4
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-13 18:28:09 -0400

    Client/build

--- server/middleware/cacheControl.js
+++
@@ -1,9 +0,0 @@
-// middleware/cacheControl.js
-module.exports = function noCache(req, res, next) {
-  res.set({
-    'Cache-Control': 'no-store',
-    'Pragma': 'no-cache',
-    'Expires': '0'
-  });
-  next();
-};
--- server/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const cwuRoutes = require('./cwu-routes');
-const ping = require('./ping')
-
-router.use('/', cwuRoutes);
-router.use('/', ping);
-
-module.exports = router;
