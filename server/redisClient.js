7578f83b0db6ba919fe0aa82b77276fd2227c211
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-14 16:37:00 -0400

    Changes to enable redis to leverage upstash cloud redis

--- server/redisClient.js
+++ server/redisClient.js
@@ -1,10 +1,6 @@
 require('dotenv').config();
 const Redis = require('ioredis');
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || '127.0.0.1',
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD,
-});
+const redis = new Redis(process.env.REDIS_URL);
 
 module.exports = redis;
