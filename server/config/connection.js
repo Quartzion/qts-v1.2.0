7fc2027d163dc5a23c216b83d5d17b41eb6d8880
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-14 18:44:51 -0400

    Resolve ssl issue for atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOURI);
+mongoose.connect(process.env.MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true
+});
 
 module.exports = mongoose.connection;
