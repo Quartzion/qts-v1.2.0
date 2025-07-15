58b34e8f59f605eb7fbcbe97adef77ad8443a01b
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-15 18:20:44 -0400

    Attempt to resolve bug-40

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 const VITE_PORT = process.env.VITE_PORT;
 const CORS_PROD = process.env.PROD_FRONTEND;
 const app = express();
-
+app.set('trust proxy', 1);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
