a0ddf72b59d88edb78a1b93ded16e0979ccc0637
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-17 23:29:47 -0400

    updated twitter card logo and added architecture diagram

--- server/swagger.js
+++ server/swagger.js
@@ -17,7 +17,7 @@ const options = {
     openapi: '3.0.0',
     info: {
       title: 'Quartzion API',
-      version: '1.0.0',
+      version: '1.2.0',
       description: 'API documentation for Quartzion Engineering',
     },
     servers: [
