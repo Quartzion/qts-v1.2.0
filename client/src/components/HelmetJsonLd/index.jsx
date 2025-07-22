054cca86abe5d9a7d06c1bfc0e1caf11fcaca433
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2025-07-22 19:40:48 -0400

    Updated HelemetJsonLd component

--- client/src/components/HelmetJsonLd/index.jsx
+++ client/src/components/HelmetJsonLd/index.jsx
@@ -2,77 +2,64 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import slugify from 'slugify';
 import blogs from '../../utils/blogData';
-import { generateBlogJsonLd } from '../../utils/generateJsonLd';
-import { useSearchParams } from 'react-router-dom';
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
 export default function HelmetJsonLd() {
-  const [searchParams] = useSearchParams();
-  const slug = searchParams.get('slug');
-//   const fullUrl = `${url}/blogs?slug=${slug}`;
-//   const baseUrl = "https://www.quartzion.com";
 
-  const structuredData = {
-    "@context": "https://schema.org",
-    "@type": "WebSite",
-    "name": "Quartzion.com",
-    "url": baseUrl,
-    "potentialAction": {
-      "@type": "SearchAction",
-      "target": `${baseUrl}/?q={search_term_string}`,
-      "query-input": "required name=search_term_string"
-    }
-  };
-
-  const expandedBlog = blogs.find(blog => slugify(blog.title, { lower: true, strict: true }) === slug);
-
-  const jsonLd = expandedBlog
-    ? generateBlogJsonLd(expandedBlog, baseUrl)
-    : {
+    const structuredData = {
         "@context": "https://schema.org",
         "@graph": [
-          structuredData,
-          ...blogs.map(blog => ({
-            "@type": "BlogPosting",
-            "headline": blog.title,
-            "description": blog.description || blog.content.slice(0, 160),
-            // "image": [`${url}${blog.img}`],
-            "author": {
-              "@type": "Person",
-              "name": "Peter Smith",
-            //   "image": `${url}/pete-ceo-2.webp`,
-              "url": `${baseUrl}/#peter-smith`,
-
+            {
+                "@type": "WebSite",
+                "name": "Quartzion.com",
+                "url": baseUrl,
+                "potentialAction": {
+                    "@type": "SearchAction",
+                    "target": `${baseUrl}/?q={search_term_string}`,
+                    "query-input": "required name=search_term_string"
+                }
             },
-            // "publisher": {
-            // "@type": "Organization",
-            // "name": "Quartzion Technology Solutions Corp.",
-            // "logo": {
-            // "@type": "ImageObject",
-            // "url": `${url}/qts-icon-2.webp`
-            // }
-            //  },
-            // "articleSection": "Technology",
-            // "articleBody": blog.content,
-            // "mainEntityOfPage": {
-            // "@type": "WebPage",
-            // "@id": fullUrl
-            // },
-            "inLanguage": "en-US",
-            "keywords": "technology, nonprofit, innovation, Quartzion, community, development, 501c3",
-            "datePublished": blog.date || "2025-07-01",
-            "url": `${baseUrl}/blogs?slug=${slugify(blog.title, { lower: true, strict: true })}`,
-            "image": `${baseUrl}${blog.img}`
-          }))
+            ...blogs.map(blog => ({
+                "@type": "BlogPosting",
+                "headline": blog.title,
+                "name": blog.title,
+                "description": blog.description || blog.content.slice(0, 160),
+                "author": {
+                    "@type": "Person",
+                    "name": "Peter Smith",
+                    "url": `${baseUrl}/#peter-smith`,
+                    "image": `${baseUrl}/pete-ceo-2.webp`
+                },
+                "publisher": {
+                    "@type": "Organization",
+                    "name": "Quartzion Technology Solutions Corp.",
+                    "logo": {
+                        "@type": "ImageObject",
+                        "url": `${baseUrl}/qts-icon-2.webp`
+                    }
+                },
+                "articleSection": "Technology",
+                "articleBody": blog.content.replace(/[\u2028\u2029]/g, ""),
+                "mainEntityOfPage": {
+                    "@type": "WebPage",
+                    "@id": `${baseUrl}/blogs?slug=${slugify(blog.title, { lower: true, strict: true })}`
+                },
+                "inLanguage": "en-US",
+                "keywords": "technology, nonprofit, innovation, Quartzion, community, development, 501c3",
+                "datePublished": blog.date || "2025-07-01",
+                "url": `${baseUrl}/blogs?slug=${slugify(blog.title, { lower: true, strict: true })}`,
+                "image": `${baseUrl}${blog.img.replace(/^\./, '')}`,
+                "wordCount": blog.content.split(/\s+/).length
+            }))
         ]
-      };
+    };
 
-  return (
-    <Helmet>
-      <script type="application/ld+json">
-        {JSON.stringify(jsonLd)}
-      </script>
-    </Helmet>
-  );
+    return (
+        <Helmet>
+            <script type="application/ld+json">
+                {JSON.stringify(structuredData)}
+            </script>
+        </Helmet>
+    );
 };
 
--- client/src/pages/Page1.jsx
+++ client/src/pages/Page1.jsx
@@ -11,59 +11,8 @@ import HelmetJsonLd from '../components/HelmetJsonLd';
 
 export default function Page1() {
 
-    // structured data JSON-LD (SEO)
-    // const structuredData = {
-    //     "@context": "https://schema.org",
-    //     "@type": "WebSite",
-    //     "name": "Quartzion.com",
-    //     "url": "https://www.quartzion.com/",
-    //     "potentialAction":
-    //     {
-    //         "@type": "SearchAction",
-    //         "target": "https://www.quartzion.com/?q={search_term_string}",
-    //         "query-input": "required name=search_term_string"
-    //     }
-    // };
-
-    // const blogPosts = [
-    //     {
-    //         "@type": "BlogPosting",
-    //         "headline": "Empowering Non-Profits with Durable Technology: Reducing Overhead & Enhancing Donor Engagement",
-    //         "author": { "@type": "Person", "name": "Peter Smith CEO" },
-    //         "datePublished": "2025-06-30T00:00:00-04:00",
-    //         "url": "https://www.quartzion.com/#blog-0",
-    //         "image": "https://www.quartzion.com/blog_2.webp"
-    //     },
-    //     {
-    //         "@type": "BlogPosting",
-    //         "headline": "Effective Tech Analysis for Seamless Project Management",
-    //         "author": { "@type": "Person", "name": "Peter Smith CEO" },
-    //         "datePublished": "2025-06-30T00:00:00-04:00",
-    //         "url": "https://www.quartzion.com/#blog-1",
-    //         "image": "https://www.quartzion.com/images/blog_1.webp"
-    //     },
-    //     {
-    //         "@type": "BlogPosting",
-    //         "headline": "Strengthening Community Through Smart Technology Integration",
-    //         "author": { "@type": "Person", "name": "Peter Smith CEO" },
-    //         "datePublished": "2025-06-30T00:00:00-04:00",
-    //         "url": "https://www.quartzion.com/#blog-2",
-    //         "image": "https://www.quartzion.com/blog_3.webp"
-    //     }
-    // ]
-
     return (
         <>
-            {/* <Helmet>
-                <title>Quartzion.com</title>
-                <link rel="canonical" href="https://www.quartzion.com" />
-                <script type="application/ld+json">
-                    {JSON.stringify({
-                        "@context": "https://schema.org",
-                        "@graph": [structuredData, ...blogPosts]
-                    })}
-                </script>
-            </Helmet> */}
             <Helmet>
                 <title>Quartzion.com</title>
                 <link rel="canonical" href="https://www.quartzion.com" />
